2123cbde3ad4c208ae0732b5b43548b2832ef9fd
Rebeca Machado <beka.machado13@poli.ufrj.br>
2025-04-01 14:44:48 -0300

    Formato da data em 4T2023 tratado, Staging dos dados para tratamento dos valores decimais.

--- 3_sql/estrutura_postgres.sql
+++ 3_sql/estrutura_postgres.sql
@@ -12,7 +12,7 @@ CREATE TABLE operadores_ativos (
     "Uf" CHAR(2) NOT NULL,
     "Cep" VARCHAR(9) NOT NULL,
     "Ddd" CHAR(2),
-    "Telefone" VARCHAR(11),
+    "Telefone" VARCHAR(20),
     "Fax" VARCHAR(15),
     "Endereco_Eletronico" VARCHAR(255),
     "Representante" VARCHAR(255) NOT NULL,
@@ -30,13 +30,22 @@ CREATE TABLE staging_demonstracoes (
     "VL_SALDO_FINAL" TEXT
 );
 
+CREATE TABLE staging_4t2023 (
+    "DATA" TEXT,  
+    "REG_ANS" TEXT,
+    "CD_CONTA_CONTABIL" TEXT,
+    "DESCRICAO" TEXT,
+    "VL_SALDO_INICIAL" TEXT, 
+    "VL_SALDO_FINAL" TEXT
+)
+
 CREATE TABLE demonstracoes_contabeis(
     "DATA" DATE,
     "TRIMESTRE" INT NOT NULL CHECK ("TRIMESTRE" BETWEEN 1 AND 4),
     "REG_ANS" INTEGER NOT NULL,
     "CD_CONTA_CONTABIL" VARCHAR(9) NOT NULL,
     "DESCRICAO" VARCHAR(255),
-    "VL_SALDO_INICIAL" DECIMAL(12,2) NOT NULL,
-    "VL_SALDO_FINAL" DECIMAL(12,2) NOT NULL,
+    "VL_SALDO_INICIAL" DECIMAL(14,2) NOT NULL,
+    "VL_SALDO_FINAL" DECIMAL(14,2) NOT NULL,
     FOREIGN KEY ("REG_ANS") REFERENCES operadores_ativos("Registro_Ans")
 )
--- 3_sql/transformar_dados.sql
+++ 3_sql/transformar_dados.sql
@@ -1,10 +1,29 @@
 INSERT INTO demonstracoes_contabeis("DATA", "REG_ANS", "CD_CONTA_CONTABIL", "DESCRICAO", "VL_SALDO_INICIAL", "VL_SALDO_FINAL", "TRIMESTRE")
 SELECT 
-    "DATA",
-    "REG_ANS"::INTEGER,  -- Se o REG_ANS for numérico na tabela final
-    "CD_CONTA_CONTABIL",
-    "DESCRICAO",
-    REPLACE("VL_SALDO_INICIAL", ',', '.')::NUMERIC,
-    REPLACE("VL_SALDO_FINAL", ',', '.')::NUMERIC,
-    EXTRACT(QUARTER FROM "DATA")  -- Se desejar calcular o trimestre a partir da data
-FROM staging_demonstracoes;
+    s."DATA",
+    s."REG_ANS"::INTEGER,
+    s."CD_CONTA_CONTABIL",
+    s."DESCRICAO",
+    REPLACE(s."VL_SALDO_INICIAL", ',', '.')::NUMERIC,
+    REPLACE(s."VL_SALDO_FINAL", ',', '.')::NUMERIC,
+    EXTRACT(QUARTER FROM s."DATA")
+FROM staging_demonstracoes s
+WHERE s."REG_ANS"::INTEGER IN (
+    SELECT "Registro_Ans"
+    FROM operadores_ativos
+);
+
+INSERT INTO demonstracoes_contabeis("DATA", "REG_ANS", "CD_CONTA_CONTABIL", "DESCRICAO", "VL_SALDO_INICIAL", "VL_SALDO_FINAL", "TRIMESTRE")
+SELECT 
+    to_date(d."DATA", 'DD/MM/YYYY'),
+    d."REG_ANS"::INTEGER,
+    d."CD_CONTA_CONTABIL",
+    d."DESCRICAO",
+    REPLACE(d."VL_SALDO_INICIAL", ',', '.')::NUMERIC,
+    REPLACE(d."VL_SALDO_FINAL", ',', '.')::NUMERIC,
+    EXTRACT(QUARTER FROM to_date(d."DATA", 'DD/MM/YYYY'))
+FROM staging_4t2023 d
+WHERE d."REG_ANS"::INTEGER IN (
+    SELECT "Registro_Ans"
+    FROM operadores_ativos
+);
